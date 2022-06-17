c596f701277080d6e8de19021d6b0d2bd2be2872
Thomas Regourd <tom.reg@hotmail.fr>
2022-06-17 08:31:19 +0200

    add layout component to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Hero from "../components/Hero"
-import Services from "../components/Services"
-import Jobs from "../components/Jobs"
-import Projects from "../components/Projects"
-import Seo from "../components/Seo"
+import React from "react";
+import { graphql } from "gatsby";
+import Hero from "../components/Hero";
+import Services from "../components/Services";
+import Jobs from "../components/Jobs";
+import Projects from "../components/Projects";
+import Seo from "../components/Seo";
+import Layout from "../components/Layout";
 const IndexPage = () => {
   return (
     <>
-      <h1>Gatsby Strapi Portfolio Starter</h1>
+      <Layout>Gatsby Strapi Portfolio Starter</Layout>
     </>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
